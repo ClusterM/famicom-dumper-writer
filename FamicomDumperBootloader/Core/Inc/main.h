dd6204f4e4b8e3f4823a1bede5967492b594fa1d
Alexey Cluster <cluster@cluster.wtf>
2024-01-06 15:55:04 +0400

    Update, refactoring

---
+++ FamicomDumperBootloader/Core/Inc/main.h
@@ -0,0 +1,94 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+void error(void);
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define TDO_Pin GPIO_PIN_0
+#define TDO_GPIO_Port GPIOC
+#define TCK_Pin GPIO_PIN_1
+#define TCK_GPIO_Port GPIOC
+#define TDI_Pin GPIO_PIN_2
+#define TDI_GPIO_Port GPIOC
+#define TMS_Pin GPIO_PIN_3
+#define TMS_GPIO_Port GPIOC
+#define M2_Pin GPIO_PIN_0
+#define M2_GPIO_Port GPIOA
+#define WS2812_Pin GPIO_PIN_2
+#define WS2812_GPIO_Port GPIOA
+#define FSMC_NOE_Pin GPIO_PIN_4
+#define FSMC_NOE_GPIO_Port GPIOD
+#define FSMC_NWE_Pin GPIO_PIN_5
+#define FSMC_NWE_GPIO_Port GPIOD
+#define FSMC_NWAIT_Pin GPIO_PIN_6
+#define FSMC_NWAIT_GPIO_Port GPIOD
+#define FSMC_NE1_Pin GPIO_PIN_7
+#define FSMC_NE1_GPIO_Port GPIOD
+#define FSMC_NE2_Pin GPIO_PIN_9
+#define FSMC_NE2_GPIO_Port GPIOG
+#define IRQ_Pin GPIO_PIN_7
+#define IRQ_GPIO_Port GPIOB
+#define SHIFTERS_OE_Pin GPIO_PIN_0
+#define SHIFTERS_OE_GPIO_Port GPIOE
+
+/* USER CODE BEGIN Private defines */
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
